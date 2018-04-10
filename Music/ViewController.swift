cabcb11b1be9cb213fbecbfdc42bef9fcc0daa4a
中山弘瑛 <hiroaki1207.gk@gmail.com>
2018-04-10 09:09:37 +0900

    MUSICアプリ完成

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func natsuPalayButton(){
+        setAudioPlayer(soundName: "natsu", type: "mp3")
+        audioPlayer.play()
+    }
+  
+    @IBAction func amePalayButton(){
+        setAudioPlayer(soundName: "ame", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func akiPalayButton(){
+        setAudioPlayer(soundName: "aki", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    
+    func setAudioPlayer(soundName: String, type: String){
+        let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        do{
+            audioPlayer = try AVAudioPlayer (contentsOf: fileURL)
+        } catch {
+            print("音楽ファイルが読み込めませんでした。")
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
